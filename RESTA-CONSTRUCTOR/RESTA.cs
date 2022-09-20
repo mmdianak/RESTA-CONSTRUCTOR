7cf46b1ee2f78102044646d7a1564eaf8e79dbbe
dk188 <dk188@LAPTOP-UVIC0HB6>
2022-09-20 11:04:44 -0600

    Agregar archivos de proyecto.

---
+++ RESTA-CONSTRUCTOR/RESTA.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTA_CONSTRUCTOR
+{
+    internal class RESTA
+    {
+        //campos, propiedades, como es 
+        private float n1, n2;
+        //constructor
+        public RESTA(float n1, float n2)
+        {
+            this.n1 = n1;
+            this.n2 = n2;
+        }
+        public float restar()
+        {
+            return (n1 - n2);
+        }
+    }
+}
